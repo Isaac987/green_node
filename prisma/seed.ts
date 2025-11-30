1654dc0ecfccb6ba40fc6cc283aebdff938bb375
Isaac Perkins <isaacperkins987@gmail.com>
2025-11-30 13:59:44 -0800

    First Prism migration with a simple plant model

---
+++ prisma/seed.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma";
+
+async function main() {
+  const plants = [
+    { name: "Monstera" },
+    { name: "Fiddle Leaf Fig" },
+    { name: "Snake Plant" },
+    { name: "Pothos" },
+    { name: "Aloe Vera" },
+  ];
+
+  console.log(`Start seeding ...`);
+
+  for (const p of plants) {
+    const plant = await prisma.plant.upsert({
+      where: { name: p.name },
+      update: {}, // No updates if it exists
+      create: {
+        name: p.name,
+      },
+    });
+    console.log(`Created plant with id: ${plant.id} and name: ${plant.name}`);
+  }
+
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
